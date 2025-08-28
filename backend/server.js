30c22446d4b31dfa733d289632ccea6f8c1029da
Tanish431 <tanishsoni431@gmail.com>
2025-08-28 16:08:00 +0530

    Updated App.jsx for server

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,13 @@ const { users, tasks } = require('./db');
 const app = express();
 const PORT = process.env.PORT || 5000;
 const JWT_SECRET = process.env.JWT_SECRET;
+const FRONTEND_URL = 'https://cctodo.netlify.app' || 'http://localhost:5173';
+app.use(
+  cors({
+    origin: FRONTEND_URL,
+    credentials: true,
+  })
+);
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -51,6 +58,14 @@ app.post('/api/auth/signin', (req, res) => {
 app.get('/api/tasks', authenticateToken, (req, res) => {
   const userTasks = tasks.filter(t => t.userId === req.user.id);
   res.json(userTasks);
+});
+
+app.get("/api/health", (req, res) => {
+  res.status(200).json({
+    status: "ok",
+    message: "Backend is running!",
+    timestamp: new Date().toISOString(),
+  });
 });
 
 app.post('/api/tasks', authenticateToken, (req, res) => {
